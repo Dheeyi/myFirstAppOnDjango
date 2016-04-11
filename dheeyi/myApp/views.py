0aa4840dd4ea4e2b72df0684303bbf4f3eb9c126
dheeyi <dheeyi@gmail.com>
2016-04-11 16:31:02 -0400

    initial commit

---
+++ dheeyi/myApp/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse, Http404
+import datetime
+
+
+# def hello(request):
+#     return HttpResponse('hola mundo')
+#
+#
+# def current_datetime(request):
+#     now = datetime.datetime.now()
+#     html = "It is now %s." % now
+#     return HttpResponse(html)
+#
+#
+# def hours_ahead(request, offset):
+#     try:
+#         offset = int(offset)
+#     except ValueError:
+#         raise Http404()
+#     dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
+#     html = "In %s hour(s), it will be %s." % (offset, dt)
+#     return HttpResponse(html)
