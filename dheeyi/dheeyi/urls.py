2cd99868e922454821542c616e302e15139a59e1
dheeyi <dheeyi@gmail.com>
2016-04-12 12:41:46 -0400

    Create a first template

--- dheeyi/dheeyi/urls.py
+++ dheeyi/dheeyi/urls.py
@@ -13,12 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
 # from myApp.views import hello, current_datetime, hours_ahead
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'', include('blog.urls')),
     # url('^hello/$', hello),
     # url('^time/$', current_datetime),
     # url(r'^time/plus/(d{1,2})/$', hours_ahead),
